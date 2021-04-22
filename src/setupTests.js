166e7c89ba07c6803712fc8679b6e3ec32e25727
David <david.gomez.redondo.dev@gmail.com>
2021-04-22 22:12:19 +0200

    intersectionalObserved mocked

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'react-intersection-observer';
+import '__mocks__/intersectionObserverMock'
 // window.IntersectionObserver = jest.fn( () => {
 //     this.observe = jest.fn();
 //     this.unobserve = jest.fn();
