58cc9bf5713bdff119a7677d9aea05b17cb77ad9
David <david.gomez.redondo.dev@gmail.com>
2021-05-01 16:52:12 +0200

    infinity scroll fixed, no more just-debounce-it

--- src/views/SearchResults/index.js
+++ src/views/SearchResults/index.js
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useCallback } from 'react';
-import debounce from 'just-debounce-it';
 import {Helmet} from 'react-helmet';
 
 import ListOfGifs from 'components/ListOfGifs';
@@ -11,20 +10,21 @@ export default function SearchResults ( {params} ) {
   const { keyword } = params;
   const externalRef = useRef();
   const { loading, gifs, setPage } = useGifs( { keyword } );
-  const { isNearScreen } = useNearScreen( { externalRef: loading ? null : externalRef, distance: '100px', once: false } );
+  const { isNearScreen } = useNearScreen( { externalRef: loading ? null : externalRef, distance: '50px', once: false } );
 
-  const debounceHandleNextPage =  useCallback(() => debounce(
-    () => setPage(prevPage => prevPage + 1), 200
-  ),[setPage])
+  const handleNextPage =  useCallback(
+    () => setPage(prevPage => prevPage + 1)
+  ,[setPage])
 
   const title = `Resultados para "${keyword}"`;
 
   useEffect(function () {
-    if(isNearScreen) debounceHandleNextPage();
-  },[isNearScreen, debounceHandleNextPage])
+    if(isNearScreen) handleNextPage();
+  },[isNearScreen, handleNextPage])
 
   return <>
-    {loading 
+    {
+    loading 
     ? <h2>Cargando ...</h2>
     : <>
         <Helmet>
